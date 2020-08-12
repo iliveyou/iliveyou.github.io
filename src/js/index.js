45df4f336d733439203cc3424543db1b21eb8cc4
Frugan <dev@frugan.it>
2020-08-12 18:22:20 +0200

    Added privacy policy page Working on contact and newsletter forms

--- src/js/index.js
+++ src/js/index.js
@@ -49,6 +49,9 @@ import "owl.carousel";
 //https://stackoverflow.com/a/45085355/3929620
 //import { Typed } from 'typed.js';
 
+//https://stackoverflow.com/a/46993250/3929620
+import '@fancyapps/fancybox';
+
 import WOW from 'wow.js';
 
 import './theme/plugins';
@@ -57,8 +60,95 @@ import './theme/init';
 (function($) {
     'use strict';
 
+    $.extend({
+        //https://stackoverflow.com/a/50319997
+        //https://jsfiddle.net/trixta/T29Kx/
+        //https://www.html5rocks.com/en/tutorials/forms/constraintvalidation/
+        //https://stackoverflow.com/a/59732941
+        checkForm: function() {
+
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation');
+
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function(form) {
+                form.addEventListener('submit', function(event) {
+
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+
+                        var errorElements = $(form).find('.form-control:invalid, .form-check-input:invalid, .custom-control-input:invalid, .custom-file-input:invalid, .custom-select:invalid');
+
+                        $.each(errorElements, function(index, element) {
+                            $(element).siblings('.invalid-feedback').html( $(element)[0].validationMessage );
+                        });
+                    }
+
+                    form.classList.add('was-validated');
+                }, false);
+            });
+
+            // before submit
+            var elements = document.querySelectorAll('.form-control, .form-check-input, .custom-control-input, .custom-file-input');
+            $.each(elements, function(index, element) {
+                if (element.willValidate === true) {
+                    $(element).keyup(function() {
+                        if (element.checkValidity() === true) {
+                            $(element).removeClass('is-invalid').addClass('is-valid');
+                        } else {
+                            $(element).removeClass('is-valid').addClass('is-invalid');
+                            $(element).siblings('.invalid-feedback').html( $(element)[0].validationMessage );
+                        }
+                    });
+                }
+            });
+
+            var elements = document.querySelectorAll('.custom-select');
+            $.each(elements, function(index, element) {
+                if (element.willValidate === true) {
+                    $(element).change(function() {
+                        if (element.checkValidity() === true) {
+                            $(element).removeClass('is-invalid').addClass('is-valid');
+                        } else {
+                            $(element).removeClass('is-valid').addClass('is-invalid');
+                            $(element).siblings('.invalid-feedback').html( $(element)[0].validationMessage );
+                        }
+                    });
+                }
+            });
+        },
+    });
+
     $(".player").YTPlayer();
 
     new WOW().init();
+
+    //starter JavaScript for disabling form submissions if there are invalid fields
+    window.addEventListener('load', function() {
+
+        $.checkForm();
+
+        //https://stackoverflow.com/a/41694352
+        $(document).on('submit', 'form[data-sync]', function (e) {
+
+            if (!e.isDefaultPrevented()) {
+                //e.preventDefault();
+
+                var $form = $(this);
+                var $btn = $form.find('button[type="submit"]');
+
+                //https://stackoverflow.com/a/54533740
+                $btn.prop('disabled', true);
+
+                var $loading_text = $btn.data('loading-text');
+
+                if (typeof $loading_text !== 'undefined') {
+
+                    $btn.html($loading_text);
+                }
+            }
+        });
+    });
 
 }(jQuery));
